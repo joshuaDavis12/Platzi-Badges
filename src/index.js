dde7267f849c205dd7557c79de937c5bec6cac6e
Abbie <jdriveraramirez@gmail.com>
2020-06-16 10:11:29 -0600

    16/6 10am First component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Badge from './components/Badge'
+
+const name = 'Mr. Joshi'
+
+const element = (
+    <div>
+        <h1>
+            Hello, I'm {name}
+        </h1>
+        <p>I'm Frontend Developer</p>
+    </div>
+)
+
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
